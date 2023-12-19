1755e286867e4f1194f8df128a46d759d76a2ce6
ivanm <ivanm@pop-os.localdomain>
2023-12-19 19:10:30 +0200

    task 2 without explaining

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # task 1
-
+print('Task 1:')
 for i in ['ecoli.fasa', 'graci2.fasta', 'pneumohern.fasta']:
-    print(i)
+    print(i, ':', sep='')
     with open('ecoli.fasta') as seq:
         data = seq.read().split('>')
         data.pop(0)
@@ -12,3 +12,17 @@ for i in ['ecoli.fasa', 'graci2.fasta', 'pneumohern.fasta']:
         codons_s.remove('')
         for i in codons_s:
             print(f'\t{i}\t{codons.count(i)}')
+
+
+# task 2
+print('Task 2:')
+with open('ecoli.fasta') as seq:
+    data = seq.read().split('>')
+    data.pop(0)
+
+for seq in data:
+    temp = ''.join(seq.split('\n')[1:])[0:-3]
+
+    for i in range(int(len(temp)/3)):
+        if temp[i*3:i*3 + 3] == 'TGA' or temp[i*3:i*3 + 3] == 'TAG' or temp[i*3:i*3 + 3] == 'TAA':
+            print('\t', seq.split('\n')[0])
