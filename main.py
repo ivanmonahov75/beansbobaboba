8059c71d649260b3150f03593730841b829a119e
ivanm <ivanm@pop-os.localdomain>
2023-12-20 12:29:58 +0200

    3 done

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # task 1
 print('Task 1:')
-for i in ['ecoli.fasa', 'graci2.fasta', 'pneumohern.fasta']:
+for i in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
     print(i, ':', sep='')
-    with open('ecoli.fasta') as seq:
+    with open(i) as seq:
         data = seq.read().split('>')
         data.pop(0)
         codons = []
         for i in data:
             codons.append(''.join(i.split('\n')[1:])[0:3])
         codons_s = sorted(list(set(codons)))
-        codons_s.remove('')
+        try:
+            codons_s.remove('')
+        except:
+            pass
         for i in codons_s:
             print(f'\t{i}\t{codons.count(i)}')
 
@@ -26,3 +29,26 @@ for seq in data:
     for i in range(int(len(temp)/3)):
         if temp[i*3:i*3 + 3] == 'TGA' or temp[i*3:i*3 + 3] == 'TAG' or temp[i*3:i*3 + 3] == 'TAA':
             print('\t', seq.split('\n')[0])
+
+print('Task 3:')
+for i in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
+    print(i, ':', sep='')
+    with open(i) as seq:
+        data = seq.read().split('>')
+        data.pop(0)
+        codons = []
+        # temp = ''.join(data[1].split('\n')[1:])
+        # print(''.join((temp[-3], temp[-2], temp[-1])))
+        for i in data:
+            temp = ''.join(i.split('\n')[1:])
+            try:
+                codons.append(''.join((temp[-3], temp[-2], temp[-1])))
+            except:
+                pass
+        codons_s = sorted(list(set(codons)))
+        try:
+            codons_s.remove('')
+        except:
+            pass
+        for i in codons_s:
+            print(f'\t{i}\t{codons.count(i)}')
