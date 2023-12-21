86fd4a431ee8b443ed134b8fba9939c9a3a87d05
ivanm <ivanm@pop-os.localdomain>
2023-12-21 18:22:18 +0200

    hotfix

--- main.py
+++ main.py
@@ -2,45 +2,47 @@ from matplotlib import pyplot as plt
 
 # task 1
 print('Task 1:')
-for i in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
-    print(i, ':', sep='')
-    with open(i) as seq:
-        data = seq.read().split('>')
-        data.pop(0)
+for bac in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
+    print(bac, ':', sep='')
+    with open(bac) as file:
+        data = file.read().split('>')[1:]
+
         codons = []
-        for i in data:
-            codons.append(''.join(i.split('\n')[1:])[0:3])
-        codons_s = sorted(list(set(codons)))
+        for seq in data:
+            codons.append(''.join(seq.split('\n')[1:])[0:3])
+
+        codons_sorted = sorted(list(set(codons)))
         try:
-            codons_s.remove('')
+            codons_sorted.remove('')
         except:
             pass
-        for i in codons_s:
-            print(f'\t{i}\t{codons.count(i)}')
+
+        for codon in codons_sorted:
+            print(f'\t{codon}\t{codons.count(codon)}')
 
 
 # task 2
 print('Task 2:')
-with open('ecoli.fasta') as seq:
-    data = seq.read().split('>')
-    data.pop(0)
+with open('ecoli.fasta') as file:
+    data = file.read().split('>')[1:]
 
-for seq in data:
-    temp = ''.join(seq.split('\n')[1:])[0:-3]
+    for seq in data:
+        temp = ''.join(seq.split('\n')[1:])[0:-3]
 
-    for i in range(int(len(temp)/3)):
-        if temp[i*3:i*3 + 3] == 'TGA' or temp[i*3:i*3 + 3] == 'TAG' or temp[i*3:i*3 + 3] == 'TAA':
-            print('\t', seq.split('\n')[0])
+        for i in range(int(len(temp)/3)):
+            if temp[i*3:i*3 + 3] == 'TGA' or temp[i*3:i*3 + 3] == 'TAG' or temp[i*3:i*3 + 3] == 'TAA':
+                print('\t', seq.split('\n')[0])
+                break
 
+# task 3
 print('Task 3:')
-for i in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
-    print(i, ':', sep='')
-    with open(i) as seq:
-        data = seq.read().split('>')
-        data.pop(0)
+for bac in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
+    print(bac, ':', sep='')
+
+    with open(bac) as seq:
+        data = seq.read().split('>')[1:]
         codons = []
-        # temp = ''.join(data[1].split('\n')[1:])
-        # print(''.join((temp[-3], temp[-2], temp[-1])))
+
         for i in data:
             temp = ''.join(i.split('\n')[1:])
             try:
@@ -52,65 +54,64 @@ for i in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
             codons_s.remove('')
         except:
             pass
-        for i in codons_s:
-            print(f'\t{i}\t{codons.count(i)}')
 
+        for codon in codons_s:
+            print(f'\t{codon}\t{codons.count(i)}')
+
+# task 4
 print('Task 4:')
-for i in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
-    print(i, ':', sep='')
-    with (open(i) as seq):
-        data = seq.read().split('>')
-        data.pop(0)
-        cod = dict.fromkeys(['CTT', 'CTC', 'CTA', 'CTG', 'TTA', 'TTG' ], 0)
+for bac in ['ecoli.fasta', 'graci2.fasta', 'pneumohern.fasta']:
+    print(bac, ':', sep='')
+
+    with open(bac) as seq:
+        data = seq.read().split('>')[1:]
+
+        cod = dict.fromkeys(['CTT', 'CTC', 'CTA', 'CTG', 'TTA', 'TTG'], 0)
         with open('out.txt', 'w') as ans:
             prot = []
             for i in data:
                 prot.append(''.join(i.split('\n')[1:]))
 
-
-            for i in range(len(prot)):
-                prot[i] = ''.join(list(prot[i])[:-3])
             prot = ''.join(prot)
+
             for i in range(int(len(prot)/3)):
                 try:
                     cod[prot[i*3:i*3 + 3]] += 1
                 except:
                     pass
 
-
-
             for i in cod.keys():
                 print(f'\t{i}\t{cod[i]}')
 
+# task 5
 print('Task 5:', sep='')
 with open('ecoli_full.fasta') as seq:
     data = seq.read().split('>')
     data.pop(0)
     data = ''.join(data[0].split('\n'))
-    with open('out.txt', 'w') as ans:
-        prot = []
-        for i in data:
-            prot.append(''.join(i.split('\n')[1:]))
+    prot = []
+    for i in data:
+        prot.append(''.join(i.split('\n')[1:]))
 
-        window, step = 100000, 1000
-        gc_sum = 0
-        # data = ''.join(prot)
-        loc = []
-        glob = []
-        for i in range(int(len(data) / step)):
-            temp = data[i * step:i * step + window]
-            if len(temp) < window:
-                break
-            try:
-                gc = (temp.count('G') - temp.count('C')) / (temp.count('G') + temp.count('C'))
-            except ZeroDivisionError:
-                gc = 0
-            gc_sum += gc
-            ans.write(f'{i * step}\t{gc:.3f}\t{gc_sum:.3f}\n')
-            loc.append(gc)
-            glob.append(gc_sum)
-        print('\t', gc_sum)
+    window, step = 100000, 1000
+    gc_sum = 0
+    loc = []
+    glob = []
+    for i in range(int(len(data) / step)):
+        temp = data[i * step:i * step + window]
+        if len(temp) < window:
+            break
+        try:
+            gc = (temp.count('G') - temp.count('C')) / (temp.count('G') + temp.count('C'))
+        except ZeroDivisionError:
+            gc = 0
+        gc_sum += gc
+        loc.append(gc)
+        glob.append(gc_sum)
+    print('\t', gc_sum)
+
 plt.figure(figsize=(30, 10))
+# un comment for second graph
 # plt.plot(loc, label='Current')
 # plt.legend()
 # plt.show()
